8ae2fb49b272de4de2935d8a27cbf64e58d6fd7c
27ssss <vinniksemen4@gmail.com>
2025-03-20 02:06:25 -0400

    preTailwindChanges

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Component } from 'react';
+import {motion} from "framer-motion"
+import camera from "../public/icons/camera-svgrepo-com1.svg"
+import castle from "../public/icons/castle-svgrepo-com.svg"
+import dots from "../public/icons/dots.svg"
+import shop from "../public/icons/shop-2-svgrepo-com.svg"
+import account from "../public/icons/account-svgrepo-com.svg"
+
+
 
 const Header = () => {
-  return <header></header>;
+  return <header>
+    <div className="container">
+      <div className='flex-row justify-between items-center text-black bg-'>
+        
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
