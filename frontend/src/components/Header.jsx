2c57e71cd9c785c7af6d37d162b9f1d2630adda2
27ssss <vinniksemen4@gmail.com>
2025-03-20 04:18:06 -0400

    navbar_almo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {motion} from "framer-motion"
 import camera from "../public/icons/camera-svgrepo-com1.svg"
 import castle from "../public/icons/castle-svgrepo-com.svg"
 import dots from "../public/icons/dots.svg"
 import shop from "../public/icons/shop-2-svgrepo-com.svg"
 import account from "../public/icons/account-svgrepo-com.svg"
-
-
-
+import logo from "../public/icons/logo123.svg"
 const Header = () => {
+  const [open, setOpen] = useState(false)
   return <header>
-    <div className="container">
-      <div className='flex-row justify-between items-center text-black bg-'>
-        
+    <div className="">
+      <div className='flex justify-evenly items-center text-black bg-[#F5F5DC] shadow py-3.5'>
+        <a href="#" className='flex justify-between items-center text-2xl font-bold gap-3'>
+          <img src={logo} alt="" className='w-[206px] h-[84px]'/>
+        </a>
+        <a href="#" className='flex justify-between items-center text-2xl font-bold gap-3'>
+          <img src={castle} alt="" className='w-11'/>
+          <p>Home</p>
+        </a>
+        <a href="#" className='flex justify-between items-center text-2xl font-bold gap-2'>
+          <img src={camera} alt="" className='w-13'/>
+          <p>Take a shot</p>
+        </a>
+        <a href="#" className='flex justify-between items-center text-2xl font-bold gap-2'>
+          <img src={shop} alt="" className='w-11'/>
+          <p>Shop</p>
+        </a>
+        <a href="#" className='flex justify-between items-center text-2xl font-bold gap-3'>
+          <img src={dots} alt="" className='w-11'/>
+          <p>More</p>
+        </a>
+        <a href="#" className='flex justify-between items-center text-2xl font-bold gap-2'>
+          <img src={account} alt="" className='w-11'/>
+          <p>Sign In | Sign Up</p>
+        </a>
       </div>
     </div>
   </header>;
