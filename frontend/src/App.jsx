a66e94d921cd7a10555ef9744568b863c30cde78
Temur Garayev <tgarayev@myseneca.ca>
2025-03-20 01:11:07 -0400

    Setup react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import AppLayout from "./components/AppLayout";
+import Error from "./components/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
 function App() {
-  return <div className="text-3xl font-bold underline">Hello World!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/components/AppLayout.jsx
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+
+      <Header />
+      <main>
+        <div className="container">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default AppLayout;
